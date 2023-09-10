a9f71ee4b0f04fb8e4248a80798638dd4248f995
Priyanshu Ganatra <priyanshuganatra@gmail.com>
2023-09-10 23:59:49 +0530

    added explanation of code

--- CS_24_FindMissingNumberInArray.cpp
+++ CS_24_FindMissingNumberInArray.cpp
@@ -1,6 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// suppose n = 5 and array: 1 2 4 5
+// then idealXOR = 1 ^ 2 ^ 3 ^ 4 ^ 5 
+// and calculatedXOR = 1 ^ 2 ^ 4 ^ 5
+// so when we do idealXOR ^ calculatedXOR, every same number will cancel out and we will be left with the missing number
+
 int missingNumber(vector<int> &a, int N)
 {
     int idealXOR = 0, calculatedXOR = 0;
@@ -8,7 +13,9 @@ int missingNumber(vector<int> &a, int N)
     for (int i = 0; i < n; i++)
     {
         calculatedXOR = calculatedXOR ^ a[i];
-        idealXOR = idealXOR ^ (i + 1);
+        // suppose n = 5, then the loop goes from 0 to 3 and the below line will calculate xor of 1 to 4, so after the
+        // loop we xor the idealXOR with 5 to get the idealXOR as 5 was not included in the loop to xor with the other numbers
+        idealXOR = idealXOR ^ (i + 1); // as the elements in array start from 1 and not 0 
     }
     idealXOR = idealXOR ^ N;
     return idealXOR ^ calculatedXOR;
@@ -19,9 +26,9 @@ int main()
     int N;
     cout << "Enter the size of the array: ";
     cin >> N;
-    vector<int> a(N-1);
+    vector<int> a(N - 1);
     cout << "Enter the elements of the array: ";
-    for (int i = 0; i < N-1; i++)
+    for (int i = 0; i < N - 1; i++)
         cin >> a[i];
     cout << "The missing number is: ";
     cout << missingNumber(a, N) << endl;

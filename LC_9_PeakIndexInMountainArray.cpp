8b6097e15f0a696eb6587a791397179c3ad5a5c6
Priyanshu Ganatra <priyanshuganatra@gmail.com>
2023-09-30 01:57:03 +0530

    added better code

--- LC_9_PeakIndexInMountainArray.cpp
+++ LC_9_PeakIndexInMountainArray.cpp
@@ -1,28 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int peakIndexInMountainArray(vector<int> &arr)
+// easy to understand
+int peakIndexInMountainArray(vector<int> &a)
 {
-    int n = arr.size();
-    int s = 0;
-    int e = n - 1;
-    while (s < e)
+    int ans = -1;
+    int start = 0;
+    int end = a.size() - 1;
+
+    while (start <= end)
     {
-        int mid = s + (e - s) / 2;
-        if (arr[mid] < arr[mid + 1])
+        int mid = (start + end) / 2;
+        // currently in A portion
+        // peak exists in right portion
+        if (a[mid + 1] > a[mid])
         {
-            // currently in A portion
-            // peak exists in right portion
-            s = mid + 1;
+            start = mid + 1;
+        }
+        // currently in B portion
+        // peak exists in left portion
+        else if (a[mid - 1] > a[mid])
+        {
+            end = mid - 1;
         }
         else
         {
-            // currently either on peak element or in B portion
-            e = mid;
+            // It must be Peak
+            ans = mid;
+            break;
         }
     }
-    return s;
+    return ans;
 }
+
+// a little bit tricky
+// int peakIndexInMountainArray(vector<int> &arr)
+// {
+//     int n = arr.size();
+//     int s = 0;
+//     int e = n - 1;
+//     while (s < e)
+//     {
+//         int mid = s + (e - s) / 2;
+//         if (arr[mid] < arr[mid + 1])
+//         {
+//             // currently in A portion
+//             // peak exists in right portion
+//             s = mid + 1;
+//         }
+//         else
+//         {
+//             // currently either on peak element or in B portion
+//             e = mid;
+//         }
+//     }
+//     return s;
+// }
 
 int main()
 {

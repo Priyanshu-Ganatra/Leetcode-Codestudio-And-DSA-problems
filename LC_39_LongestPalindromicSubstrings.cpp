1d180e506bf0e70728eda04d260182ea540a3539
Priyanshu Ganatra <priyanshuganatra@gmail.com>
2023-10-03 01:22:26 +0530

    solved on 03/10/23

---
+++ LC_39_LongestPalindromicSubstrings.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPalindrome(string &s, int start, int end){
+    while(start < end){
+        if(s[start] != s[end])
+            return false;
+        start++;
+        end--;
+    }
+    return true;
+}
+
+string longestPalindrome(string s)
+{
+    string ans = "";
+    for (int i = 0; i < s.size(); i++)
+    {
+        for (int j = i; j < s.size(); j++)
+        {
+            if (isPalindrome(s, i, j))
+            {
+                string temp = s.substr(i, j - i + 1);
+                if (temp.size() > ans.size())
+                    ans = temp;
+            }
+            
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    string s = "babad";
+    cout << longestPalindrome(s) << endl;
+    return 0;
+}

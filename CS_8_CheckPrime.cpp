8e4f2ac428987bf693e18fa4c27456420e72414e
Priyanshu Ganatra <priyanshuganatra@gmail.com>
2023-10-04 19:08:01 +0530

    added proper indentation

--- CS_8_CheckPrime.cpp
+++ CS_8_CheckPrime.cpp
@@ -1,27 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool isPrime(int n) {
-  int cnt = 0;
-  for (int i = 1; i <= sqrt(n); i++) {
-	if (n == 1) return false;
-    if (n % i == 0) {
-      cnt++;
-    }
-    if (cnt > 1) break;  // Break early if more than 1 divisor is found
-  }
-  if (cnt == 1)  // A prime number should have exactly 2 divisors: 1 and itself
-    return true;
-  else
-    return false;
+bool isPrime(int n)
+{
+	int cnt = 0;
+	for (int i = 1; i <= sqrt(n); i++)
+	{
+		if (n == 1)
+			return false;
+		if (n % i == 0)
+		{
+			cnt++;
+		}
+		if (cnt > 1)
+			break; // Break early if more than 1 divisor is found
+	}
+	if (cnt == 1) // A prime number should have exactly 2 divisors: 1 and itself
+		return true;
+	else
+		return false;
 }
 
-int main(){
-    int n = 11;
-    if(isPrime(n)){
-        cout << "Prime";
-    } else {
-        cout << "Not Prime";
-    }
-    return 0;
+int main()
+{
+	int n = 11;
+	if (isPrime(n))
+	{
+		cout << "Prime";
+	}
+	else
+	{
+		cout << "Not Prime";
+	}
+	return 0;
 }

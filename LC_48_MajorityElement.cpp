5266dd9ef00165a3c64cef1931f8db7852036dfd
Priyanshu Ganatra <priyanshuganatra@gmail.com>
2023-10-05 23:28:09 +0530

    solved on 05/10/23

--- LC_48_MajorityElement.cpp
+++ LC_48_MajorityElement.cpp
@@ -4,7 +4,7 @@ using namespace std;
 // Boyer-Moore Voting Algorithm O(n) time O(1) space
 int majorityElement(vector<int> &nums)
 {
-    int counter = 0, majority;
+    int counter = 0, majority = -1;
     for (int num : nums)
     {
         if (!counter)
@@ -18,7 +18,8 @@ int majorityElement(vector<int> &nums)
 
 int main()
 {
-    vector<int> nums = {3, 2, 3};
+    vector<int> nums = {3, 2, 2}; // correct test case
+    // vector<int> nums = {3, 3, 2, 2}; // incorrect test case
     cout << majorityElement(nums) << endl;
     return 0;
 }

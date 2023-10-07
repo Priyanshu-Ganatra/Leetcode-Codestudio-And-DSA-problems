717fbe9fd7faf5f4d4ed9a64fe415cb859f068fd
Priyanshu Ganatra <priyanshuganatra@gmail.com>
2023-10-08 01:45:05 +0530

    solved on 07/10/23

--- LC_51_MinTimeDifference.cpp
+++ LC_51_MinTimeDifference.cpp
@@ -1,47 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int getTime(string &time)
+int minDiff(vector<int> &arr)
 {
-    int hours = stoi(time.substr(0, 2));
-    int mins = stoi(time.substr(3));
-    int timeInMins = (hours * 60) + mins;
-    return timeInMins;
+    int diff = INT_MAX;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (i + 1 < arr.size() && diff > arr[i + 1] - arr[i])
+        {
+            diff = arr[i + 1] - arr[i];
+        }
+    }
+    return diff;
 }
 
 int findMinDifference(vector<string> &timePoints)
 {
-    vector<bool> timeSeen(1440, false);
-    for (auto &timepoint : timePoints)
+    int ans;
+    vector<int> time;
+    for (int i = 0; i < timePoints.size(); i++)
     {
-        int timeInMins = getTime(timepoint);
-
-        // If current time is already seen, return 0.
-        if (timeSeen[timeInMins])
-            return 0;
-        timeSeen[timeInMins] = true;
+        int hr = stoi(timePoints[i].substr(0, 2));
+        int min = stoi(timePoints[i].substr(3, 2));
+        time.push_back((hr * 60) + min);
     }
-
-    // Find minimum time difference
-    int minTimeDiff = INT_MAX, firstTime = INT_MIN, prevTime = -1;
-
-    for (int i = 0; i < 1440; i++)
-    {
-        if (timeSeen[i])
-        {
-            if (firstTime == INT_MIN)
-            {
-                firstTime = i;
-                prevTime = i;
-            }
-            else
-            {
-                minTimeDiff = min({minTimeDiff, i - prevTime, 1440 - i + prevTime}); // 1440 - i + prevTime is for the case when i is less than prevTime and we need to consider the circular nature of the clock.
-                prevTime = i; // Update prevTime
-            }
-        }
-    }
-    return min({minTimeDiff, prevTime - firstTime, 1440 - prevTime + firstTime}); // 1440 - prevTime + firstTime is for the case when prevTime is less than firstTime and we need to consider the circular nature of the clock.
+    sort(time.begin(), time.end());
+    return min(minDiff(time), ((1440) + time[0]) - time[time.size() - 1]);
 }
 
 int main()

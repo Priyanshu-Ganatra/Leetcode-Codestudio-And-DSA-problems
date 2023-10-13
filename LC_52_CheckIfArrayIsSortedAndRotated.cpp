cc9ccb5c736da315fabb266fe85a1ea398943fcf
Priyanshu Ganatra <priyanshuganatra@gmail.com>
2023-10-13 18:15:17 +0530

    Added explanation on 13/10/23

--- LC_52_CheckIfArrayIsSortedAndRotated.cpp
+++ LC_52_CheckIfArrayIsSortedAndRotated.cpp
@@ -5,6 +5,7 @@ bool check(vector<int> &nums)
 {
     int n = nums.size();
     int cnt = 0;
+    // this checks if the array is sorted or not
     for (int i = 1; i < n; i++)
     {
         if (nums[i - 1] > nums[i])
@@ -12,10 +13,12 @@ bool check(vector<int> &nums)
             cnt++;
         }
     }
+    // this checks if the array is rotated or not
     if (nums[n - 1] > nums[0])
     {
         cnt++;
     }
+    // if cnt   is less than or equal to 1 then the array is sorted and rotated because if the array is sorted then cnt will be 0 and if the array is rotated then cnt will be 1
     return cnt <= 1;
 }
 

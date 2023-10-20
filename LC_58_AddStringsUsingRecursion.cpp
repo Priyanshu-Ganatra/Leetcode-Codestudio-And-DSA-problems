6efd5e42d35e95710d22b5ee2b8511397236017e
Priyanshu Ganatra <priyanshuganatra@gmail.com>
2023-10-20 17:52:39 +0530

    solved on 20/10/23

---
+++ LC_58_AddStringsUsingRecursion.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void addRE(string &num1, int p1, string &num2, int p2, string &ans, int carry = 0)
+{
+    // base case
+    if (p1 < 0 && p2 < 0)
+    {
+        if (carry != 0)
+        {
+            ans.push_back(carry + '0');
+        }
+        return;
+    }
+
+    // calculation
+    // if p1 is valid then take the value else take 0
+    int n1 = (p1 >= 0 ? num1[p1] : '0') - '0';
+    // if p2 is valid then take the value else take 0
+    int n2 = (p2 >= 0 ? num2[p2] : '0') - '0';
+    int csum = n1 + n2 + carry;
+    int digit = csum % 10;
+    carry = csum / 10;
+    ans.push_back(digit + '0');
+
+    // recursive call
+    addRE(num1, p1 - 1, num2, p2 - 1, ans, carry);
+}
+
+string addStrings(string num1, string num2)
+{
+    string ans = "";
+    addRE(num1, num1.length() - 1, num2, num2.length() - 1, ans);
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+
+int main()
+{
+    string num1 = "11";
+    string num2 = "11";
+    cout << "Sum of " << num1 << " and " << num2 << " is: ";
+    cout << addStrings(num1, num2) << endl;
+    return 0;
+}

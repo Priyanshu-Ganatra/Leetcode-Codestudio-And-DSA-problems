bb02706bdd0029c7e047322574d06db644339018
Priyanshu Ganatra <priyanshuganatra@gmail.com>
2023-10-20 16:51:10 +0530

    solved on 20/10/23

---
+++ ReverseAStringUsingRecursion.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// tc : O(n) & sc : O(n)
+void reverse(string &str, int s, int e){
+    if(s >= e){
+        return;
+    }
+    swap(str[s], str[e]);
+    reverse(str, s + 1, e - 1);
+}
+
+int main(){
+    cout << "Enter a string: ";
+    string str;
+    cin >> str;
+    reverse(str, 0, str.length() - 1);
+    cout << "Reversed string: " << str;
+    return 0;
+}

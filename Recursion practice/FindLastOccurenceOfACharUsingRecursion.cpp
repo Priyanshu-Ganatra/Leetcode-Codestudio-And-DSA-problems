5f8c38be199e056fe37356eff4c0d044d9cf8e93
Priyanshu Ganatra <priyanshuganatra@gmail.com>
2023-10-20 16:50:22 +0530

    solved on 17/10/23

--- Recursion practice/FindLastOccurenceOfACharUsingRecursion.cpp
+++ Recursion practice/FindLastOccurenceOfACharUsingRecursion.cpp
@@ -1,17 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// tc : O(n) & sc : O(n)
+// left to right
+// void solve(string &s, char c, int i, int &ans)
+// {
+//     // base case
+//     if (i >= s.length())
+//         return;
+//     // calculation
+//     if (s[i] == c)
+//     {
+//         ans = i;
+//     }
+//     solve(s, c, i + 1, ans);
+// }
+
+// tc : O(n) & sc : O(n)
+// right to left 
 void solve(string &s, char c, int i, int &ans)
 {
     // base case
-    if (i >= s.length())
+    if (i < 0)
         return;
     // calculation
     if (s[i] == c)
     {
         ans = i;
+        return;
     }
-    solve(s, c, i + 1, ans);
+    solve(s, c, i - 1, ans);
 }
 
 int main()
@@ -19,8 +37,9 @@ int main()
     string str = "abcabcabc";
     char c = 'c';
     int ans = -1;
-    solve(str, c, 0, ans);
-    cout << "Last occurence of " << c << " is at index: ";
+    // solve(str, c, 0, ans); // left to right
+    solve(str, c, str.length() - 1, ans); // right to left 
+    cout << "Last occurence of " << c << " in string \"" << str << "\" is at index: ";
     cout << ans;
     return 0;
 }

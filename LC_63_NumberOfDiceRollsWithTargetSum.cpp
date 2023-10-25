fd8f1dc846d78dfcaefff1501df1f9129a818e19
Priyanshu Ganatra <priyanshuganatra@gmail.com>
2023-10-25 23:53:35 +0530

    solved on 25/10/23

---
+++ LC_63_NumberOfDiceRollsWithTargetSum.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// gives TLE for large inputs, we'll use DP to optimize it in future
+// copilot is trained on some wild data💀💀
+int numRollsToTarget(int n, int k, int target)
+{
+    // base cases
+    if (target < 0)
+        return 0;
+    if (n == 0 && target != 0)
+        return 0;
+    if (n == 0 && target == 0)
+        return 1;
+
+    /// calculation
+    int ans = 0;
+    for (int i = 1; i <= k; i++)
+    {
+        ans = ans + numRollsToTarget(n - 1, k, target - i);
+    }
+    return ans;
+}
+
+int main()
+{
+    return 0;
+}

6bf17af8461e46dc6361afb9e7fac86fdef99928
Priyanshu Ganatra <priyanshuganatra@gmail.com>
2023-10-28 02:41:14 +0530

    solved on 28/10/23

--- LC_65_IntegerToEnglishWords.cpp
+++ LC_65_IntegerToEnglishWords.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 vector<pair<int, string>> mp = {{{1000000000, "Billion"}, {1000000, "Million"}, {1000, "Thousand"}, {100, "Hundred"}, {90, "Ninety"}, {90, "Ninety"}, {80, "Eighty"}, {70, "Seventy"}, {60, "Sixty"}, {50, "Fifty"}, {40, "Forty"}, {30, "Thirty"}, {20, "Twenty"}, {19, "Nineteen"}, {18, "Eighteen"}, {17, "Seventeen"}, {16, "Sixteen"}, {15, "Fifteen"}, {14, "Fourteen"}, {13, "Thirteen"}, {12, "Twelve"}, {11, "Eleven"}, {10, "Ten"}, {9, "Nine"}, {8, "Eight"}, {7, "Seven"}, {6, "Six"}, {5, "Five"}, {4, "Four"}, {3, "Three"}, {2, "Two"}, {1, "One"}}};
+
+// tc : O(log₁₀n) & sc : O(log₁₀n) as the number of digits in the number is log₁₀n where n is the number itself
 string numberToWords(int num)
 {
     // base cases

4503fd767632178400697942c818df421177b826
Priyanshu Ganatra <priyanshuganatra@gmail.com>
2023-12-27 23:13:22 +0530

    added optimal solution on 27/12/23

--- LC_111_BalancedBinaryTree.cpp
+++ LC_111_BalancedBinaryTree.cpp
@@ -11,57 +11,82 @@ struct TreeNode
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-// height using level order traversal
+// // height using level order traversal
+// int height(TreeNode *root)
+// {
+//     if (root == NULL)
+//         return 0;
+//     queue<TreeNode *> q;
+//     q.push(root);
+//     int height = 0;
+//     while (!q.empty())
+//     {
+//         int s = q.size();
+//         for (int i = 0; i < s; i++)
+//         {
+//             TreeNode *node = q.front();
+//             q.pop();
+//             if (node->left != NULL)
+//                 q.push(node->left);
+//             if (node->right != NULL)
+//                 q.push(node->right);
+//         }
+//         height++;
+//     }
+//     return height;
+// }
+
+// // brute force approach
+// // tc : O(n^2) sc : O(n)
+// bool isBalanced(TreeNode *root)
+// {
+//     // base case
+//     if (root == NULL)
+//     {
+//         return true;
+//     }
+
+//     // solve for current node
+//     int leftHeight = height(root->left);
+//     int rightHeight = height(root->right);
+//     int diff = abs(leftHeight - rightHeight);
+
+//     bool currNodeAns = (diff <= 1);
+
+//     // solve for left subtree
+//     bool leftSubtreeAns = isBalanced(root->left);
+
+//     // solve for right subtree
+//     bool rightSubtreeAns = isBalanced(root->right);
+
+//     // final answer
+//     bool ans = currNodeAns && leftSubtreeAns && rightSubtreeAns;
+//     return ans;
+// }
+
+bool balance = true;
 int height(TreeNode *root)
 {
-    if (root == NULL)
+    if (!root)
         return 0;
-    queue<TreeNode *> q;
-    q.push(root);
-    int height = 0;
-    while (!q.empty())
+    int lh = height(root->left);
+    int rh = height(root->right);
+
+    // check current node, is it balanced?
+    if (balance && abs(lh - rh) > 1)
     {
-        int s = q.size();
-        for (int i = 0; i < s; i++)
-        {
-            TreeNode *node = q.front();
-            q.pop();
-            if (node->left != NULL)
-                q.push(node->left);
-            if (node->right != NULL)
-                q.push(node->right);
-        }
-        height++;
+        balance = false;
     }
-    return height;
+
+    // return current node height
+    return max(lh, rh) + 1;
 }
 
-// brute force approach
-// tc : O(n^2) sc : O(n)
+// Optimal, tc: O(n), sc: O(n)
 bool isBalanced(TreeNode *root)
 {
-    // base case
-    if (root == NULL)
-    {
-        return true;
-    }
-
-    // solve for current node
-    int leftHeight = height(root->left);
-    int rightHeight = height(root->right);
-    int diff = abs(leftHeight - rightHeight);
-
-    bool currNodeAns = (diff <= 1);
-
-    // solve for left subtree
-    bool leftSubtreeAns = isBalanced(root->left);
-
-    // solve for right subtree
-    bool rightSubtreeAns = isBalanced(root->right);
-
-    // final answer
-    bool ans = currNodeAns && leftSubtreeAns && rightSubtreeAns;
-    return ans;
+    height(root);
+    return balance;
 }
 
 int main()

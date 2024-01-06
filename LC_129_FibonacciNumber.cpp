e2c9f6cb8083e3f8c56edffdb54a8ff58ed0bb89
Priyanshu Ganatra <priyanshuganatra@gmail.com>
2024-01-06 23:30:05 +0530

    solved on 6/1/24

--- LC_129_FibonacciNumber.cpp
+++ LC_129_FibonacciNumber.cpp
@@ -20,29 +20,55 @@ using namespace std;
 //     return dp[n];
 // }
 
-int solveUsingTabulation(int n)
+// bottom-up tabulation - 1d dp
+// w/o space optimization
+// tc - O(n) and sc - O(n)
+// int solveUsingTabulation(int n)
+// {
+//     // step 1 - create dp array
+//     vector<int> dp(n + 1, -1);
+//     // step 2 - analyse base case & fill dp array
+//     dp[0] = 0;
+//     if (n == 0)
+//         return dp[0];
+//     dp[1] = 1;
+//     // step 3 - fill the remaining dp array
+//     // array size = n+1
+//     // indexes upto => n
+//     // 0 & 1 indexes are alread filled
+//     // remaining indexes 2 -> n
+//     for (int i = 2; i <= n; i++)
+//     {
+//         // copy paste the recursive logic
+//         // replace recursive calls with dp array
+//         // make sure indexing in dp array is using looping variable "i"
+//         dp[i] = dp[i - 1] + dp[i - 2];
+//     }
+//     // return ans
+//     return dp[n];
+// }
+
+// bottom-up tabulation - 1d dp
+// w/ space optimization
+// tc - O(n) and sc - O(1)
+int solveUsingTabulationSpaceOpti(int n)
 {
-    // step 1 - create dp array
-    vector<int> dp(n + 1, -1);
-    // step 2 - analyse base case & fill dp array
-    dp[0] = 0;
-    if (n == 0)
-        return dp[0];
-    dp[1] = 1;
-    // step 3 - fill the remaining dp array
-    // array size = n+1
-    // indexes upto => n
-    // 0 & 1 indexes are alread filled
-    // remaining indexes 2 -> n
+    // step 1 - analyse base case & fill dp variables (prev & curr)
+    int prev = 0;
+    if (n == 0 || n == 1)
+        return n;
+    int curr = 1;
+    int ans = 0;
+    // step 2 - update the dp variables and find ans using them
     for (int i = 2; i <= n; i++)
     {
-        // copy paste the recursive logic
-        // replace recursive calls with dp array
-        // make sure indexing in dp array is using looping variable "i"
-        dp[i] = dp[i - 1] + dp[i - 2];
+        ans = curr + prev;
+        // step 3 - update prev & curr
+        prev = curr;
+        curr = ans;
     }
     // return ans
-    return dp[n];
+    return ans;
 }
 
 int fib(int n)
@@ -50,7 +76,8 @@ int fib(int n)
     // // step 1 - create dp array (Memoization)
     // vector<int> dp(n+1, -1);
     // return solveUsingMemo(n, dp);
-    return solveUsingTabulation(n);
+    // return solveUsingTabulation(n);
+    return solveUsingTabulationSpaceOpti(n);
 }
 
 int main()

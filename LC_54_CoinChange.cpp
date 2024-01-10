8505362c83a2ed051e00b752348305b115173f3d
Priyanshu Ganatra <priyanshuganatra@gmail.com>
2024-01-10 18:08:43 +0530

    added dp solution on 10/1/24

--- LC_54_CoinChange.cpp
+++ LC_54_CoinChange.cpp
@@ -1,43 +1,119 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int solve(vector<int> &coins, int amount)
-{
-    // base case
-    if (amount == 0)
-        return 0;
-    int minimumAmountOfCoins = INT_MAX;
-    if (amount < 0)
-        return INT_MAX; // INT_MAX is returned to avoid the case when (amount is not divisible by any of the coins)/(when the amount is negative)
+// tc: O(n^amount) sc: O(n)
+// int solveUsingRecursion(vector<int> &coins, int amount)
+// {
+//     // base cases
+//     // if amount = 0 then I don't need to use no coin so I return 0
+//     if (amount == 0)
+//         return 0;
+//     int minimumAmountOfCoins = INT_MAX;
+//     // when the amount becomes negative we return minimumAmountOfCoins i.e. INT_MAX
+//     if (amount < 0)
+//         return minimumAmountOfCoins;
 
-    for (int coin : coins)
+//     for (int coin : coins)
+//     {
+//         // only use the current coin if its value is less than or =
+//         // the current amount, else the amount will be calculated
+//         // in negative
+//         if (coin <= amount)
+//         {
+//             int recAns = solveUsingRecursion(coins, amount - coin);
+//             // if the amount is not possible to make with the current coin
+//             // then INT_MAX will be returned by the recursive call and we
+//             // don't want to include that in our answer so we'll skip it and
+//             // move to the next coin in the coins array to check if it is
+//             // possible to make the amount with that coin or not
+//             if (recAns != minimumAmountOfCoins)
+//             {
+//                 int ans = 1 + recAns; // 1 is added to include the current coin
+//                 minimumAmountOfCoins = min(minimumAmountOfCoins, ans);
+//             }
+//         }
+//     }
+//     return minimumAmountOfCoins;
+// }
+
+// tc: O(n * amount) sc: O(n)
+// int solveUsingMemo(vector<int> &coins, int amount, vector<int> &dp)
+// {
+//     // base cases
+//     // if amount = 0 then I don't need to use no coin so I return 0
+//     if (amount == 0)
+//         return 0;
+//     int minimumAmountOfCoins = INT_MAX;
+//     // when the amount becomes negative we return minimumAmountOfCoins i.e. INT_MAX
+//     if (amount < 0)
+//         return minimumAmountOfCoins;
+
+//     // if ans already exists then return it
+//     if (dp[amount] != -1)
+//         return dp[amount];
+
+//     for (int coin : coins)
+//     {
+//         // only use the current coin if its value is less than or =
+//         // the current amount, else the amount will be calculated
+//         // in negative
+//         if (coin <= amount)
+//         {
+//             int recAns = solveUsingMemo(coins, amount - coin, dp);
+//             // if the amount is not possible to make with the current coin
+//             // then INT_MAX will be returned by the recursive call and we
+//             // don't want to include that in our answer so we'll skip it and
+//             // move to the next coin in the coins array to check if it is
+//             // possible to make the amount with that coin or not
+//             if (recAns != INT_MAX)
+//             {
+//                 int ans = 1 + recAns; // 1 is added to include the current coin
+//                 minimumAmountOfCoins = min(minimumAmountOfCoins, ans);
+//             }
+//         }
+//     }
+//     dp[amount] = minimumAmountOfCoins;
+//     return dp[amount];
+// }
+
+// tc: O(n * amount) sc: O(n)
+int solveUsingTabulation(vector<int> &coins, int &amount, vector<int> &dp)
+{
+    int n = amount; // n is the amount
+    dp[0] = 0; // base case
+    for (int i = 1; i <= n; i++) // i is the current amount
     {
-        // only use the current coin if its value is less than or =
-        // the current amount, else the amount will be calculated
-        // in negative
-        if (coin <= amount)
+        int minimumAmountOfCoins = INT_MAX; // minimum amount of coins required to make the current amount
+        // for each coin in the coins array check if it is possible to make the current amount with it or not
+        for (int coin : coins)
         {
-            int recAns = solve(coins, amount - coin);
-            // if the amount is not possible to make with the current coin then INT_MAX will be returned by the recursive call and we don't want
-            // to include that in our answer so we'll skip it and move to the next coin in the coins array to check if it is possible to make the
-            // amount with that coin or not
-            if (recAns != INT_MAX)
+            // only use the current coin if its value is less than or = the current amount, else the amount will be calculated in negative
+            if (coin <= i)
             {
-                int ans = 1 + recAns; // 1 is added to include the current coin
-                minimumAmountOfCoins = min(minimumAmountOfCoins, ans);
+                int minCoinsTillNow = dp[i - coin]; // minimum amount of coins required to make the amount till now
+                if (minCoinsTillNow != INT_MAX) 
+                {
+                    int ans = 1 + minCoinsTillNow;
+                    minimumAmountOfCoins = min(minimumAmountOfCoins, ans);
+                }
             }
         }
+        dp[i] = minimumAmountOfCoins;
     }
-    return minimumAmountOfCoins; // this will be triggered when none of the coins can be used to make the amount
+    return dp[n];
 }
 
-// recursive solution, gives TLE on leetcode. we'll use DP to optimize it in future
 int coinChange(vector<int> &coins, int amount)
 {
-    int ans = solve(coins, amount);
+    // int ans = solveUsingRecursion(coins, amount);
+    int n = amount;
+    vector<int> dp(n + 1, -1);
+    // int ans = solveUsingMemo(coins, amount, dp);
+    int ans = solveUsingTabulation(coins, amount, dp);
     if (ans == INT_MAX)
         return -1;
-    return ans;
+    else
+        return ans;
 }
 
 int main()
--- LC_55_HouseRobber.cpp
+++ LC_55_HouseRobber.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// tc: O(2^n) sc: O(n)
 int solve(vector<int> &nums, int n, int idx)
 {
     // base case
@@ -19,12 +20,92 @@ int solve(vector<int> &nums, int n, int idx)
     return ans;
 }
 
-// recursive solution, gives TLE on leetcode. we'll use DP to optimize it in future
-// tc: O(2^n) sc: O(n)
+// tc: O(2^n) sc: O(n).
+// int solveUsingRecursion(vector<int>& nums, int n, int idx) {
+//     // base case
+//     if(idx >= n){
+//         return 0;
+//     }
+
+//     // rob this house
+//     int opt1 = nums[idx] + solveUsingRecursion(nums, n, idx + 2);
+
+//     // don't rob this house
+//     int opt2 = 0 + solve(nums, n, idx + 1);
+
+//     int ans = max(opt1, opt2);
+//     return ans;
+// }
+
+// tc: O(n) sc: O(n)
+// int solveUsingMemo(vector<int> &nums, int &n, int idx, vector<int> &dp)
+// {
+//     // base case
+//     if (idx >= n)
+//     {
+//         return 0;
+//     }
+
+//     // if ans already exists then return it
+//     if (dp[idx] != -1)
+//         return dp[idx];
+
+//     // rob this house
+//     int opt1 = nums[idx] + solveUsingMemo(nums, n, idx + 2, dp);
+
+//     // don't rob this house
+//     int opt2 = 0 + solveUsingMemo(nums, n, idx + 1, dp);
+
+//     dp[idx] = max(opt1, opt2);
+//     return dp[idx];
+// }
+
+// tc: O(n) sc: O(n)
+// int solveUsingTabulation(vector<int> &nums, int &n, vector<int> &dp)
+// {
+//     dp[n - 1] = nums[n - 1];
+//     for (int i = n - 2; i >= 0; i--)
+//     {
+//         int tempAns = 0;
+//         if (i + 2 < n)
+//             tempAns = dp[i + 2];
+//         int rob = nums[i] + tempAns;
+//         int dontRob = 0 + dp[i + 1];
+//         dp[i] = max(rob, dontRob);
+//     }
+//     return dp[0];
+// }
+
+// tc: O(n) sc: O(1)
+int solveUsingTabulationSpaceOpti(vector<int> &nums, int &n)
+{
+    int prev = nums[n - 1];
+    int prevPrev = 0;
+    int curr = 0;
+    for (int i = n - 2; i >= 0; i--)
+    {
+        int tempAns = 0;
+        if (i + 2 < n)
+            tempAns = prevPrev;
+        int rob = nums[i] + tempAns;
+        int dontRob = 0 + prev;
+        curr = max(rob, dontRob);
+
+        // shifting pointers back
+        prevPrev = prev;
+        prev = curr;
+    }
+    return prev;
+}
+
 int rob(vector<int> &nums)
 {
     int n = nums.size();
-    return solve(nums, n, 0);
+    // return solveUsingRecursion(nums, n, 0);
+    vector<int> dp(n, -1);
+    // return solveUsingMemo(nums, n, 0, dp);
+    // return solveUsingTabulation(nums, n, dp);
+    return solveUsingTabulationSpaceOpti(nums, n);
 }
 
 int main()

564a259697efddf781c5b6bcde4673c17e255c3a
Priyanshu Ganatra <priyanshuganatra@gmail.com>
2024-02-04 22:59:18 +0530

    added heapify on 04/02/24

--- Heaps/MaxHeap.cpp
+++ Heaps/MaxHeap.cpp
@@ -83,6 +83,32 @@ public:
         }
         return ans;
     }
+
+    // O(n) - this method corrects the heap property of the array
+    void heapify(vector<int> &arr, int n, int index){
+        int leftIndex = 2 * index;
+        int rightIndex = 2 * index + 1;
+        int maxIndex = index;
+
+        // check if left child exists and is greater than parent
+        if (leftIndex <= n && arr[leftIndex] > arr[maxIndex])
+        {
+            maxIndex = leftIndex;
+        }
+        // check if right child exists and is greater than parent and left child
+        if (rightIndex <= n && arr[rightIndex] > arr[maxIndex])
+        {
+            maxIndex = rightIndex;
+        }
+
+        // if maxIndex is not same as index, then swap and heapify
+        if (maxIndex != index)
+        {
+            swap(arr[maxIndex], arr[index]);
+            index = maxIndex; // update index to max child because we need to heapify from there
+            heapify(arr, n, index);
+        }
+    }
 };
 
 int main()
@@ -106,5 +132,17 @@ int main()
         cout << h.arr[i] << " ";
     }
     cout << endl;
+    cout << "Heapify this incorrect heap:" << endl;
+    vector<int> v = {0, 45, 50, 60, 40, 30, 20};
+    for (int i = 1; i <= 6; i++)
+    {
+        cout << v[i] << " ";
+    }
+    h.heapify(v, 4, 1);
+    cout << endl << "Printing array after heapify:" << endl;
+    for (int i = 1; i <= 6; i++)
+    {
+        cout << v[i] << " ";
+    }
     return 0;
 }

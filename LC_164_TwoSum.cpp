8691007d9aff592deab48c16103d9612e1f3e939
Priyanshu Ganatra <priyanshuganatra@gmail.com>
2024-05-12 00:02:40 +0530

    solved on 11/05/24

--- LC_164_TwoSum.cpp
+++ LC_164_TwoSum.cpp
@@ -4,6 +4,7 @@ using namespace std;
 class Solution
 {
 public:
+    // tc : O(nlogn) & sc : O(n)
     vector<int> twoSum(vector<int> &nums, int target)
     {
         vector<pair<int, int>> temp;
@@ -35,6 +36,25 @@ public:
             }
         }
         return {};
+    }
+
+    // tc : O(n) & sc : O(n)
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> numMap;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++)
+        {
+            int complement = target - nums[i];
+            if (numMap.count(complement))
+            {
+                return {numMap[complement], i};
+            }
+            numMap[nums[i]] = i;
+        }
+
+        return {}; // No solution found
     }
 };
 

a8e5b895e04e18b66e3e530f464c1df46a180562
Priyanshu Ganatra <priyanshuganatra@gmail.com>
2024-05-19 23:38:37 +0530

    updated on 19/05/24

--- BitManipulation.cpp
+++ BitManipulation.cpp
@@ -39,14 +39,27 @@ int countSetBits(int n)
     return count;
 }
 
+int clearBitsInRange(int n, int i, int j)
+{
+    int mask1 = (-1 << i);
+    int mask2 = ~(-1 << j);
+    int mask = mask1 | mask2;
+    return n & mask;
+}
+
 int main()
 {
-    int n = 10, i = 2;
+    int n = 10, i = 2, j = 3;
     cout << "Bit no. " << i << " from the right for the number " << n << " is " << getithBit(n, i) << "\n";
     i = 1;
     cout << "Setting bit no. " << i << " from the right for the number " << n << " gives " << setithBit(n, i) << "\n";
     cout << "Clearing bit no. " << i << " from the right for the number " << n << " gives " << clearithBit(n, i) << "\n";
     updateithBit(n, i, 1);
     cout << "Number of set bits in " << n << " is " << countSetBits(n) << "\n";
+    i = 4;
+    // example n = 10 i.e. 1010
+    // i and j are counted from the right-hand side so i or j = 1 means the rightmost bit 
+    // clear bits from 4 to 3 i.e. 1010 -> 0010, so ans = 2
+    cout << "Clearing bits from " << i << " to " << j << " for the number " << n << " gives " << clearBitsInRange(n, i, j) << "\n";
     return 0;
 }

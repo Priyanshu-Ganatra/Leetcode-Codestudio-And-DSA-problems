0cad85100b57a84e64fbb7f981be95a66ade84e8
Priyanshu Ganatra <priyanshuganatra@gmail.com>
2024-05-22 23:19:34 +0530

    solved on 22/05/24

--- LC_172_SudokuSolver.cpp
+++ LC_172_SudokuSolver.cpp
@@ -20,7 +20,9 @@ public:
                 return false;
 
             // 3*3 box check
-            if (board[3 * (row / 3) + (i / 3)][3 * (col / 3) + (i % 3)] == value)
+            int startRow = 3 * (row / 3);
+            int startCol = 3 * (col / 3);
+            if (board[startRow + (i / 3)][startCol + (i % 3)] == value)
                 return false;
         }
         // place is safe

0a5617fa33ed7f3fbe215e97b4ef323c6e01025d
Priyanshu Ganatra <priyanshuganatra@gmail.com>
2024-06-02 13:49:59 +0530

    updated on 02/06/24

--- LC_10_SearchInRotatedSortedArray.cpp
+++ LC_10_SearchInRotatedSortedArray.cpp
@@ -1,87 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int findPivot(vector<int> &nums)
-{
-    int s = 0;
-    int n = nums.size();
-    int e = n - 1;
-    // Example (This function also works for non-rotated sorted array):
-    // 2 4 6 8 10 - arr
-    // 0 1 2 3 4 - index
-    // ans = 4, as 10 i.e. pivot is at index 4
-    while (s <= e)
-    {
-        int mid = s + (e - s) / 2;
-        // corner case
-        if (s == e)
-        {
-            return s;
+class Solution {
+public:
+    int bs(vector<int>& nums, int s, int e, int target) {
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                s = mid + 1;
+            else
+                e = mid - 1;
         }
+        return -1;
+    }
 
-        if (mid + 1 < n && nums[mid] > nums[mid + 1])
-        {
-            return mid;
-        }
-        else if (mid - 1 >= 0 && nums[mid - 1] > nums[mid])
-        {
-            return mid - 1;
-        }
-        else if (nums[s] > nums[mid])
-        { // we're currently on portion B
-            // ans is in left portion
-            e = mid - 1;
-        }
-        else
-        { // we're currently on portion A
-            // ans is in right portion
-            s = mid + 1;
+    int search(vector<int>& nums, int target) {
+        auto max_idx = max_element(nums.begin(), nums.end());
+        int pivot = std::distance(nums.begin(), max_idx);
+
+        if (nums[0] <= target && target <= nums[pivot]) {
+            return bs(nums, 0, pivot, target);
+        } else {
+            return bs(nums, pivot + 1, nums.size() - 1, target);
         }
     }
-    return -1;
-}
+};
 
-int binarySearch(vector<int> &arr, int s, int e, int target)
-{
-    while (s <= e)
-    {
-        int mid = s + (e - s) / 2;
-        if (arr[mid] == target)
-            return mid;
-        else if (target > arr[mid])
-        {
-            s = mid + 1;
-        }
-        else
-        {
-            e = mid - 1;
-        }
-    }
-    return -1;
-}
-
-int search(vector<int> &arr, int target)
-{
-    int pivot = findPivot(arr);
-    int n = arr.size();
-    int ans = -1;
-
-    // search in A
-    if (target >= arr[0] && target <= arr[pivot])
-    {
-        ans = binarySearch(arr, 0, pivot, target);
-    }
-    else
-    {
-        ans = binarySearch(arr, pivot + 1, n - 1, target);
-    }
-    return ans;
-}
-
-int main()
-{
-    vector<int> arr = {4, 5, 6, 7, 0, 1, 2};
-    int target = 7;
-    cout << search(arr, target) << endl;
+int main() {
+    Solution s;
+    vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+    int target = 0;
+    cout << s.search(nums, target) << endl;
     return 0;
 }

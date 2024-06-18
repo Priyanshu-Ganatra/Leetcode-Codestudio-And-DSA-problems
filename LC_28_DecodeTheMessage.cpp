8815c5303e50455a2bb144a334d73731b1df9566
Priyanshu Ganatra <priyanshuganatra@gmail.com>
2024-06-18 16:06:04 +0530

    updated on 18/06/24

--- LC_28_DecodeTheMessage.cpp
+++ LC_28_DecodeTheMessage.cpp
@@ -1,41 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string decodeMessage(string key, string message)
-{
-    // create mapping (encoding)
-    char mapping[256] = {0};
-    char start = 'a';
-    for (char c : key)
-    {
-        if (c != ' ' && mapping[c] == 0)
-        {
-            mapping[c] = start;
-            start++;
-        }
-    }
+class Solution {
+public:
+    string decodeMessage(string key, string message) {
+        unordered_map<char, char> m;
+        int c = 0;
 
-    // use mapping (decoding)
-    string ans;
-    for (char ch : message)
-    {
-        if (ch == ' ')
-        {
-            ans.push_back(' ');
-        }
-        else
-        {
-            char decodedChar = mapping[ch];
-            ans.push_back(decodedChar);
-        }
-    }
-    return ans;
-}
+        for (int i = 0; i < key.size(); i++) {
+            if (!isalpha(key[i])) continue; // Ignore non-alphabetic characters
+            if (m.find(key[i]) != m.end()) continue; // Skip already mapped characters
 
-int main()
-{
-    string key = "the quick brown fox jumps over the lazy dog";
-    string message = "vkbs bs t suepuv";
-    cout << decodeMessage(key, message) << endl;
+            m[key[i]] = 'a' + c;
+            c++;
+            if (c == 26) break; // All letters are mapped
+        }
+
+        string ans = "";
+        for (auto it : message) {
+            if (isalpha(it)) {
+                ans.push_back(m[it]);
+            } else {
+                ans.push_back(it); // Keep spaces and other non-alphabetic characters 
+                // unchanged
+            }
+        }
+        return ans;
+    }
+};
+
+int main() {
+    Solution s;
+    string key = "abc";
+    string message = "abc";
+    cout << s.decodeMessage(key, message) << endl;
     return 0;
 }

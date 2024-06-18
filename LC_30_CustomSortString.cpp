9e3d172d3bcf9b9d01893b7a5778842ba8bc11e8
Priyanshu Ganatra <priyanshuganatra@gmail.com>
2024-06-18 23:56:22 +0530

    updated on 18/06/24

--- LC_30_CustomSortString.cpp
+++ LC_30_CustomSortString.cpp
@@ -1,39 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// tc: O(n*log(n)) | sc: O(m), where m = size of order string
-class Solution
-{
-public:
-    static string str;
-    // custom comparator
-    static bool compare(char ch1, char ch2)
-    {
-        // true when position of ch1 in str < position of ch2 in given order string
-        return (str.find(ch1) < str.find(ch2));
-        // when true is returned, ch1 will be placed before ch2 in the o/p string else it'll be placed after ch2
-    }
+// // tc: O(n*log(n)) | sc: O(m), where m = size of order string
+// class Solution
+// {
+// public:
+//     static string str;
+//     // custom comparator
+//     static bool compare(char ch1, char ch2)
+//     {
+//         // true when position of ch1 in str < position of ch2 in given order string
+//         return (str.find(ch1) < str.find(ch2));
+//         // when true is returned, ch1 will be placed before ch2 in the o/p string else it'll be placed after ch2
+//     }
 
-    string customSortString(string order, string s)
-    {
-        str = order;
-        sort(s.begin(), s.end(), compare);
-        return s;
+//     string customSortString(string order, string s)
+//     {
+//         str = order;
+//         sort(s.begin(), s.end(), compare);
+//         return s;
+//     }
+// };
+
+// string Solution::str;
+
+class Solution {
+public:
+    // tc and sc: O(n)
+    string customSortString(string order, string s) {
+        string result = "";
+        unordered_map<char, int> mp;  // Map to count occurrences of each character in s
+        
+        // Count the occurrences of each character in s
+        for (auto &it : s) {
+            mp[it]++;
+        }
+        
+        // Append characters in the order specified by the order string
+        for (auto &it : order) {
+            if (mp.find(it) != mp.end()) {
+                result.append(mp[it], it);
+                mp.erase(it);  // Remove character from map after processing
+            }
+        }
+        
+        // Append remaining characters that were not in the order string
+        for (auto &it : mp) {
+            result.append(it.second, it.first);
+        }
+        
+        return result;
     }
 };
 
-string Solution::str;
-
 int main()
 {
-    Solution solution;
+    Solution obj;
     string order = "cba";
     string s = "abcd";
-
-    string sortedString = solution.customSortString(order, s);
-
-    cout << "Original String: " << s << endl;
-    cout << "Sorted String: " << sortedString << endl;
-
+    cout << obj.customSortString(order, s) << endl;
     return 0;
 }

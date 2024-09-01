de4cc99224da1d1d43533192f708e1172dece83e
Priyanshu Ganatra <priyanshuganatra@gmail.com>
2024-09-01 19:31:14 +0530

    made sc O(1)

--- CS_29_CommonElementsIn3SortedArrays.cpp
+++ CS_29_CommonElementsIn3SortedArrays.cpp
@@ -1,63 +1,71 @@
+//{ Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
 
-// tc : O(n1 + n2 + n3) sc : O(min(n1 + n2 + n3))
-vector<int> findCommonElements(vector<int> &a, vector<int> &b, vector<int> &c)
-{
-    vector<int> ans;
-    set<int> s;
-    int i, j, k;
-    i = j = k = 0;
-    int n1 = a.size();
-    int n2 = b.size();
-    int n3 = c.size();
-    while (i < n1 && j < n2 && k < n3)
-    {
-        if (a[i] == b[j] && b[j] == c[k])
-        {
-            s.insert(a[i]);
-            i++, j++, k++;
-        }
-        else if (a[i] < b[j])
-        {
-            i++;
-        }
-        else if (b[j] < c[k])
-        {
-            j++;
-        }
-        else
-        {
-            k++;
-        }
-    }
-    for (auto i : s)
-    {
-        ans.push_back(i);
-    }
-    return ans;
-}
 
-int main()
-{
-    cout << "Array 1: ";
-    vector<int> a = {1, 5, 10, 20, 40, 80};
-    for (int i = 0; i < a.size(); i++)
-        cout << a[i] << " ";
-    cout << endl;
-    cout << "Array 2: ";
-    vector<int> b = {6, 7, 20, 80, 100};
-    for (int i = 0; i < b.size(); i++)
-        cout << b[i] << " ";
-    cout << endl;
-    cout << "Array 3: ";
-    vector<int> c = {3, 4, 15, 20, 30, 70, 80, 120};
-    for (int i = 0; i < c.size(); i++)
-        cout << c[i] << " ";
-    cout << endl;
-    cout << "Common elements: ";
-    vector<int> ans = findCommonElements(a, b, c);
-    for (int i = 0; i < ans.size(); i++)
-        cout << ans[i] << " ";
-    return 0;
-} 
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to find common elements in three arrays.
+    // tc: O(n1 + n2 + n3) and sc: O(1) because we are not using any extra space except the answer vector
+    vector<int> commonElements(vector<int> &A, vector<int> &B,
+                               vector<int> &C) {
+        //code here.
+        int i=0,j=0,k=0;
+        int n1=A.size(),n2=B.size(),n3=C.size();
+        vector<int> ans;
+        int last = 0;
+        while(i<n1 && j<n2 && k<n3){
+            if(A[i] == B[j] && B[j] == C[k] && A[i] != last){
+                ans.push_back(A[i]);
+                last = A[i];
+                i++; j++; k++;
+            }
+            else if(min({A[i], B[j], C[k]}) == A[i]) i++;
+            else if(min({A[i], B[j], C[k]}) == B[j]) j++;
+            else k++;
+        }
+        return ans;
+
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr, brr, crr;
+        string input1;
+        getline(cin, input1);
+        stringstream ss1(input1);
+        int number1;
+        while (ss1 >> number1) {
+            arr.push_back(number1);
+        }
+        string input2;
+        getline(cin, input2);
+        stringstream ss2(input2);
+        int number2;
+        while (ss2 >> number2) {
+            brr.push_back(number2);
+        }
+        string input3;
+        getline(cin, input3);
+        stringstream ss3(input3);
+        int number3;
+        while (ss3 >> number3) {
+            crr.push_back(number3);
+        }
+        Solution ob;
+        vector<int> res = ob.commonElements(arr, brr, crr);
+        if (res.size() == 0)
+            cout << -1;
+        for (int i = 0; i < res.size(); i++)
+            cout << res[i] << " ";
+        cout << endl;
+    }
+}
+// } Driver Code Ends
